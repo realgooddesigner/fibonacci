0715fa96153e12e842d68ec96c7e4847b596a8b2
yawner <info@yawnercreative.com>
2018-09-29 16:30:11 -0400

    Adding descriptions to markup

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 const Arena = require('are.na');
-debugger;
+
 router.get('/', (req, res, next) => {
   const arena = new Arena({
-        accessToken: '905ec405a2507d74d0c7597b6c314ba53ff6b98156e007a0b24f8e17a054ddda',
+        accessToken: process.env.PERSONAL_ACCESS_TOKEN ,
   });
-  console.log(process.env.ARENA_CHANNEL_ID);
+
   arena
-    .channel('fibonacci-vrfozkn_7t4').get()
+    .channel(process.env.ARENA_CHANNEL_ID).get()
     .then(data =>
       res.render('index', data)
     )
     .catch(next);
+    
 });
 
 module.exports = router;
