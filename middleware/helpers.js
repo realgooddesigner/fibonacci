b93abf486381151da126e32d295425b5c3de12db
yawner <info@yawnercreative.com>
2018-10-01 18:07:35 -0400

    Adding fibonacci function in js

--- middleware/helpers.js
+++ middleware/helpers.js
@@ -15,10 +15,25 @@ const shuffle = (xs) => {
 const rand = (min, max) =>
   Math.floor(Math.random() * (max - min) + min);
 
+const fibonacci = () => {
+  let i;
+  let fib = []; // Initialize array!
+debugger;
+  fib[0] = 0;
+  fib[1] = 1;
+  for (i = 2; i <= 10; i++) {
+    // Next fibonacci number = previous + one before previous
+    // Translated to JavaScript:
+    fib[i] = fib[i - 2] + fib[i - 1];
+    console.log(fib[i]);
+  };
+}
+
 module.exports = (req, res, next) => {
   res.locals.helpers = {
     shuffle,
     rand,
+    fibonacci,
   };
 
   next();
